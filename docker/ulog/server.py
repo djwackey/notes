4b257766093009b66f216595e3376f75b448717d
wackey <worcy_kiddy@126.com>
2016-04-09 23:00:26 +0800

    add ulog server

---
+++ docker/ulog/server.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import traceback
+
+from flask import Flask
+
+from werkzeug.contrib.fixers import ProxyFix
+
+from scribe import scribe
+from thrift.transport import TTransport
+from thrift.transport import TSocket
+from thrift.protocol import TBinaryProtocol
+
+
+app = Flask(__name__)
+
+@app.route('/sendbi/<string:host>/<int:port>/<string:category>/<string:message>')
+def send_bi(host, port, category, message):
+    assert message
+    assert category
+
+    if (message is None) or (category is None):
+        return "error args"
+
+    result = 0
+    socket = TSocket.TSocket(host = host, port = port)
+    socket.setTimeout(3000)
+    transport = TTransport.TFramedTransport(socket)
+    protocol = TBinaryProtocol.TBinaryProtocol(trans = transport, strictRead = False, strictWrite = False)
+    client = scribe.Client(iprot = protocol, oprot = protocol)
+
+    try:
+        transport.open()
+        log_entry = scribe.LogEntry(category = category, message = message)
+        result = client.Log(messages=[log_entry])
+    except Exception as e:
+        result = "failed to write bilog"
+        traceback.print_exc(file=sys.stdout)
+    finally:
+        try:
+            transport.close()
+        except Exception as e:
+            result = "failed to close socket"
+            traceback.print_exc(file=sys.stdout)
+
+    if result == scribe.ResultCode.OK:
+        result = "success for sending bi log"
+
+    return result
+
+
+if __name__ == '__main__':
+
+    # using wsgi
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
---
+++ docker/ulog/tasks.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+import time
+import requests
+from celery import Celery
+
+celery = Celery('tasks', broker='redis://localhost:6379/0')
+
+BASE_URL = "http://localhost:5002"
+
+DEST_HOST = "127.0.0.1" # bi server
+DEST_PORT = "1463"
+
+@celery.task
+def sendbi(category, message):
+    result = False
+    try:
+        url = "{BASE_URL}/sendbi/{DEST_HOST}/{DEST_PORT}/{category}/{message}".format(
+                BASE_URL = BASE_URL,
+                DEST_HOST = DEST_HOST,
+                DEST_PORT = DEST_PORT,
+                category = category,
+                message = message)
+
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            print(resp.content)
+            result = True
+    except Exception as e:
+        print(e)
+    return result
