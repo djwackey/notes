d881021cf011ffeb638401451d6c8c9ad691af16
wackey <worcy_kiddy@126.com>
2017-05-09 23:44:34 +0800

    Add list reverse algorithm

---
+++ algorithm/list_reverse.py
@@ -0,0 +1,8 @@
+l = [1, 2, 3, 4, 5, 6]
+print "using reverse() function to reverse list"
+l.reverse()
+print l
+
+print "using slice to reverse list"
+print "l[::-1] [start:stop:step]"
+print l[::-1]
